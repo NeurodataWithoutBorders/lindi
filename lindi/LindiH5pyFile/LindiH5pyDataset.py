b339260b983995cc7ed15b738c1c24b5034c283e
rly <rly@lbl.gov>
2024-03-20 23:05:44 -0700

    Raise error if dataset selection has non-reference dicts

--- lindi/LindiH5pyFile/LindiH5pyDataset.py
+++ lindi/LindiH5pyFile/LindiH5pyDataset.py
@@ -192,16 +192,18 @@ class LindiH5pyDataset(h5py.Dataset):
 
 def _resolve_references(x: Any):
     if isinstance(x, dict):
+        # x should only be a dict when x represents a converted reference
         if '_REFERENCE' in x:
             return LindiH5pyReference(x['_REFERENCE'])
-        else:
-            for k, v in x.items():
-                x[k] = _resolve_references(v)
+        else:  # pragma: no cover
+            raise Exception(f"Unexpected dict in selection: {x}")
     elif isinstance(x, list):
+        # Replace any references in the list with the resolved ref in-place
         for i, v in enumerate(x):
             x[i] = _resolve_references(v)
     elif isinstance(x, np.ndarray):
         if x.dtype == object or x.dtype is None:
+            # Replace any references in the object array with the resolved ref in-place
             view_1d = x.reshape(-1)
             for i in range(len(view_1d)):
                 view_1d[i] = _resolve_references(view_1d[i])
