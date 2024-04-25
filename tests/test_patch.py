f914cedb4d6e9b0a1df9a91542434c4371ac0b26
Jeremy Magland <jmagland@flatironinstitute.org>
2024-04-25 15:24:57 -0400

    update test_patch.py

--- tests/test_patch.py
+++ tests/test_patch.py
@@ -1,22 +1,24 @@
 import lindi.h5py_patch  # noqa: F401
+import tempfile
 import numpy as np
 import h5py
 from h5py import File
 
 
 def test_patch():
-    fname = 'test.lindi.json'
-    with File(fname, 'w') as f:
-        assert isinstance(f, File)
-        f.create_dataset('data', data=np.arange(500000, dtype=np.uint32), chunks=(100000,))
+    with tempfile.TemporaryDirectory() as tmpdir:
+        fname = f'{tmpdir}/test.lindi.json'
+        with File(fname, 'w') as f:
+            assert isinstance(f, File)
+            f.create_dataset('data', data=np.arange(500000, dtype=np.uint32), chunks=(100000,))
 
-    with File(fname, 'r') as f:
-        ds = f['data']
-        assert isinstance(ds, h5py.Dataset)
-        assert ds.shape == (500000,)
-        assert ds.chunks == (100000,)
-        assert ds.dtype == np.uint32
-        assert np.all(ds[:] == np.arange(500000, dtype=np.uint32))
+        with File(fname, 'r') as f:
+            ds = f['data']
+            assert isinstance(ds, h5py.Dataset)
+            assert ds.shape == (500000,)
+            assert ds.chunks == (100000,)
+            assert ds.dtype == np.uint32
+            assert np.all(ds[:] == np.arange(500000, dtype=np.uint32))
 
 
 if __name__ == '__main__':
