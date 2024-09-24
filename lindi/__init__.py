0533a668b61a41edba21110e713b4e7d3a4f0f63
rly <rly@lbl.gov>
2024-09-24 12:42:02 -0700

    Use __all__ in lindi/__init__.py

--- lindi/__init__.py
+++ lindi/__init__.py
@@ -1,5 +1,25 @@
-from .LindiH5ZarrStore import LindiH5ZarrStore, LindiH5ZarrStoreOpts  # noqa: F401
-from .LindiH5pyFile import LindiH5pyFile, LindiH5pyGroup, LindiH5pyDataset, LindiH5pyHardLink, LindiH5pySoftLink  # noqa: F401
-from .LocalCache.LocalCache import LocalCache, ChunkTooLargeError  # noqa: F401
-from .LindiRemfile.additional_url_resolvers import add_additional_url_resolver  # noqa: F401
-from .LindiH5pyFile.LindiH5pyReference import LindiH5pyReference  # noqa: F401
+from .LindiH5ZarrStore import LindiH5ZarrStore, LindiH5ZarrStoreOpts
+from .LindiH5pyFile import (
+    LindiH5pyFile,
+    LindiH5pyGroup,
+    LindiH5pyDataset,
+    LindiH5pyHardLink,
+    LindiH5pySoftLink,
+    LindiH5pyReference,
+)
+from .LocalCache.LocalCache import LocalCache, ChunkTooLargeError
+from .LindiRemfile.additional_url_resolvers import add_additional_url_resolver
+
+__all__ = [
+    "LindiH5ZarrStore",
+    "LindiH5ZarrStoreOpts",
+    "LindiH5pyFile",
+    "LindiH5pyGroup",
+    "LindiH5pyDataset",
+    "LindiH5pyHardLink",
+    "LindiH5pySoftLink",
+    "LindiH5pyReference",
+    "LocalCache",
+    "ChunkTooLargeError",
+    "add_additional_url_resolver",
+]
